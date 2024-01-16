07d8a5038b072b645e85701a2b128ecf18eab539
angelique <angeliquedhartog@gmail.com>
2024-01-16 21:21:31 +0100

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import h5py
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from tensorflow.keras.models import Sequential
@@ -48,25 +48,33 @@ def load_data(folder_path):
     data = scaler.fit_transform(data.reshape(-1, data.shape[-1])).reshape(data.shape)
     return data, labels
 
-'''
+
 def create_cnn_rnn_model(input_shape, num_task_types):
     model = Sequential()
 
     # add CNN layers here?
-    model.add(Conv2D(16, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Flatten())
+    
+    model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(1,248,8906), padding='same'))
+   
+    model.add(MaxPooling2D((2, 2), padding='same'))
+  
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+  
+    model.add(MaxPooling2D((2, 2), padding='same'))
+   
+    #model.add(Flatten())
 
+    model.summary()
     time_step = 248
-    # LSTM
-    model.add(Reshape((time_step, -1)))
+    # LSTM wants [batch, timesteps, feature(vec?)]
+    model.add(Reshape(( 248, 8)))
+    model.summary()
     model.add(LSTM(32, return_sequences=True))
     model.add(Dropout(0.5))
     model.add(LSTM(16))
     model.add(Dropout(0.5))
     model.add(Dense(num_task_types, activation='softmax'))
+    model.summary()
     print("Model created")
     return model
 
@@ -82,12 +90,12 @@ X_cross_train, y_cross_train = load_data(cross_train_path)
 X_cross_test, y_cross_test = load_data(cross_test_path)
 
 # had to do that cause apparently the data before wasnt the right format for the LSTM
-X_cross_train = np.transpose(X_cross_train, (0, 2, 1))
-X_cross_test = np.transpose(X_cross_test, (0, 2, 1))
-X_intra_train = np.transpose(X_intra_train, (0, 2, 1))
-X_intra_test = np.transpose(X_intra_test, (0, 2, 1))
+X_cross_train = np.transpose(X_cross_train, (0, 2, 1)).reshape((X_cross_train.shape[0],1, X_cross_train.shape[1],X_cross_train.shape[2] ))
+X_cross_test = np.transpose(X_cross_test, (0, 2, 1)).reshape((X_cross_test.shape[0],1,X_cross_test.shape[1],X_cross_test.shape[2]))
+X_intra_train = np.transpose(X_intra_train, (0, 2, 1)).reshape((X_intra_train.shape[0],1,X_intra_train.shape[1],X_intra_train.shape[2]))
+X_intra_test = np.transpose(X_intra_test, (0, 2, 1)).reshape((X_intra_test.shape[0],1,X_intra_test.shape[1],X_intra_test.shape[2]))
 
-input_shape_cnn = (X_intra_train.shape[1], X_intra_train.shape[2], 1)
+input_shape_cnn = (X_intra_train.shape[0], X_intra_train.shape[1],X_intra_train.shape[2])#,1)
 cnn_rnn_model = create_cnn_rnn_model(input_shape_cnn, num_task_types=4)
 cnn_rnn_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
@@ -97,8 +105,8 @@ intra_early_stopping_cb = EarlyStopping(patience=10, restore_best_weights=True)
 cross_checkpoint_cb = ModelCheckpoint("best_model.h5", save_best_only=True)
 cross_early_stopping_cb = EarlyStopping(patience=10, restore_best_weights=True)
 
-history_intra = cnn_rnn_model.fit(X_intra_train, y_intra_train, epochs=10, batch_size=8, validation_data=(X_intra_test, y_intra_test), callbacks=[intra_checkpoint_cb, intra_early_stopping_cb], verbose=1)
-history_cross = cnn_rnn_model.fit(X_cross_train, y_cross_train, epochs=10, batch_size=8, validation_data=(X_cross_test, y_cross_test), callbacks=[cross_checkpoint_cb, cross_early_stopping_cb], verbose=1)
+history_intra = cnn_rnn_model.fit(X_intra_train, y_intra_train, epochs=10,validation_data=(X_intra_test, y_intra_test), callbacks=[intra_checkpoint_cb, intra_early_stopping_cb], verbose=1)
+history_cross = cnn_rnn_model.fit(X_cross_train, y_cross_train, epochs=10, validation_data=(X_cross_test, y_cross_test), callbacks=[cross_checkpoint_cb, cross_early_stopping_cb], verbose=1)
 print("Done")
 
 final_train_accuracy = history_cross.history['accuracy'][-1]
@@ -126,7 +134,7 @@ plt.ylabel('Accuracy')
 plt.legend()
 
 plt.show()
-'''
+
 
 def create_cnn_model(input_shape, num_task_types):
     model = Sequential()
